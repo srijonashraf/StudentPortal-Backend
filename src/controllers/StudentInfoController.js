06191916faf2ec767e5bb655ec5f399446194df1
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2023-11-19 11:58:12 +0600

    Fetch Info By StuId Added

--- src/controllers/StudentInfoController.js
+++ src/controllers/StudentInfoController.js
@@ -3,9 +3,15 @@ const StudentInfoModel = require("../models/StudentInfo");
 exports.allStudents = async (req, res) => {
   try {
     let result = await StudentInfoModel.find();
-    res.status(200).json({ status: "success", message: "All Students", data: result });
+    res
+      .status(200)
+      .json({ status: "success", message: "All Students", data: result });
   } catch (err) {
-    res.status(500).json({ status: "error", message: "Server Error fetching Students Info", data: err });
+    res.status(500).json({
+      status: "error",
+      message: "Server Error fetching Students Info",
+      data: err,
+    });
   }
 };
 
@@ -13,8 +19,48 @@ exports.registration = async (req, res) => {
   let reqBody = req.body;
   try {
     let result = await StudentInfoModel.create(reqBody);
-    res.status(201).json({ status: "success", message: "Student Registered Successfully", data: result });
+    res.status(201).json({
+      status: "success",
+      message: "Student Registered Successfully",
+      data: result,
+    });
   } catch (error) {
-    res.status(400).json({ status: "error", message: "Failed to Register Student", data: error });
+    res.status(400).json({
+      status: "error",
+      message: "Failed to Register Student",
+      data: error,
+    });
+  }
+};
+
+exports.stuInfoById = async (req, res) => {
+  try {
+    // Use req.params to get the parameters from the URL
+    let id = req.params.id;
+
+    // Assuming you want to find a single student by ID, you can use findOne instead of find
+    let result = await StudentInfoModel.findOne({ id: id });
+
+    if (result) {
+      // If a student is found, return success
+      res.status(200).json({
+        status: "success",
+        message: "Student info:",
+        data: result,
+      });
+    } else {
+      // If no student is found, return a failure status
+      res.status(404).json({
+        status: "error",
+        message: "Student not found",
+      });
+    }
+  } catch (error) {
+    // Handle other errors
+    res.status(500).json({
+      status: "error",
+      message: "Failed to retrieve student information",
+      data: error,
+    });
   }
 };
--- src/routes/api.js
+++ src/routes/api.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const StudentInfoController = require('../controllers/StudentInfoController')
 
 router.get("/student/all",StudentInfoController.allStudents)
+router.get("/student/stuInfoById/:id",StudentInfoController.stuInfoById);
 router.post("/student/registration",StudentInfoController.registration);
 
 module.exports = router;
